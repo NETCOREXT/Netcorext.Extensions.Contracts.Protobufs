37eb46066067d87abcbaa533b00cbee93e72b315
Aren Chen <gucci313031@gmail.com>
2022-07-01 17:49:22 +0800

    First Release

---
+++ Extensions/ValueTypeExtension.cs
@@ -0,0 +1,29 @@
+using System.Globalization;
+
+namespace Netcorext.Contracts.Protobufs;
+
+public static class ValueTypeExtension
+{
+    public static ProtobufDecimal ToProtobufDecimal(this decimal value)
+    {
+        var decimalString = value.ToString(CultureInfo.InvariantCulture).Split('.');
+        var precision = long.Parse(decimalString[0]);
+        var scale = int.Parse(decimalString[1]);
+        
+        return new ProtobufDecimal
+               {
+                   Precision = precision,
+                   Scale = scale
+               };
+    }
+
+    public static decimal ToDecimal(this ProtobufDecimal value)
+    {
+        var precision = value.Precision;
+        var scale = value.Scale;
+        var scaleLength = value.Scale.ToString().Length;
+        var scaleFactor = (decimal)Math.Pow(10, scaleLength);
+
+        return precision + scale / scaleFactor;
+    }
+}
